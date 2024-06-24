0688d8a67f09c623491c68e56c1af31591e6487f
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-06-24 14:15:43 +0200

    Bootswatch and bootstrap, category visibility

--- PiccoloWeb/Migrations/20240624115140_SeedCategoryTable.Designer.cs
+++ PiccoloWeb/Migrations/20240624115140_SeedCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PiccoloWeb.Data;
 
@@ -10,9 +11,11 @@ using PiccoloWeb.Data;
 namespace PiccoloWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240624115140_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

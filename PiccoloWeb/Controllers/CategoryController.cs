36178d1b24e1a22a6bec0a01fd92b71e64e78ea3
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-06-24 14:51:29 +0200

    Validations

--- PiccoloWeb/Controllers/CategoryController.cs
+++ PiccoloWeb/Controllers/CategoryController.cs
@@ -24,7 +24,11 @@ namespace PiccoloWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if(ModelState.IsValid)
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The display order cannot exactly match the Name.");
+            //}
+            if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();

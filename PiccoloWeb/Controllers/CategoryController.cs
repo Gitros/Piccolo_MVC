f9ba4d2936564d7815e27a6e9dd0ef0e3f972fa1
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-06-24 15:14:31 +0200

    edit and delete

--- PiccoloWeb/Controllers/CategoryController.cs
+++ PiccoloWeb/Controllers/CategoryController.cs
@@ -24,10 +24,10 @@ namespace PiccoloWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            //if (obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("name", "The display order cannot exactly match the Name.");
-            //}
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order cannot exactly match the Name.");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
@@ -35,6 +35,56 @@ namespace PiccoloWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id== 0) 
+            { 
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult EditPOST(int? id)
+        {
+            Category? obj = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
